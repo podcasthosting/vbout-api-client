d8eb4dd9adde5d32bfb5b13b2ddeb1050d2e195e
fabio <fabio@podcast.de>
2025-01-02 10:26:37 +0100

    Fix the path to the config file from the Serviceprovider

--- src/Providers/VboutServiceProvider.php
+++ src/Providers/VboutServiceProvider.php
@@ -10,7 +10,7 @@ class VboutServiceProvider extends ServiceProvider
     public function register()
     {
         // Config publishen
-        $this->mergeConfigFrom(__DIR__.'/../../config/vbout.php', 'vbout');
+        $this->mergeConfigFrom(__DIR__.'/../config/vbout.php', 'vbout');
 
         // Haupt-Klasse als Singleton binden, damit wir sie überall injecten können
         $this->app->singleton(Vbout::class, function ($app) {
@@ -23,7 +23,7 @@ class VboutServiceProvider extends ServiceProvider
         // Erlauben, dass User die config publisht: php artisan vendor:publish --tag=vbout-config
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../../config/vbout.php' => config_path('vbout.php'),
+                __DIR__.'/../config/vbout.php' => config_path('vbout.php'),
             ], 'vbout-config');
         }
     }
