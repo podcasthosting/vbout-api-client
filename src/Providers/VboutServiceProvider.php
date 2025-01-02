932595bb754bc3527a61ea81e8027f08bd910858
fabio <fabio@podcast.de>
2025-01-02 10:24:12 +0100

    Added a Laravel service provider

---
+++ src/Providers/VboutServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace podcasthosting\VboutApiClient\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use podcasthosting\VboutApiClient\Vbout;
+
+class VboutServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Config publishen
+        $this->mergeConfigFrom(__DIR__.'/../../config/vbout.php', 'vbout');
+
+        // Haupt-Klasse als Singleton binden, damit wir sie überall injecten können
+        $this->app->singleton(Vbout::class, function ($app) {
+            return new Vbout(config('vbout.api_key'));
+        });
+    }
+
+    public function boot()
+    {
+        // Erlauben, dass User die config publisht: php artisan vendor:publish --tag=vbout-config
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../../config/vbout.php' => config_path('vbout.php'),
+            ], 'vbout-config');
+        }
+    }
+}
--- src/config/vbout.php
+++ src/config/vbout.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * This is used in conjunction with Laravel and the VbooutServiceProvider
+ * If you use a framework other than Laravel this is not most likely not interesting for you
+ */
 return [
     'url'     => env('VBOUT_URL', 'https://api.vbout.com'),
     'api_key' => env('VBOUT_API_KEY', ''),
